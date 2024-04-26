fedbd281870b19882b8d2aa2770d4ca3c57ca08b
zzudongxiang <zzudongxiang@163.com>
2024-04-26 09:40:25 +0800

    宏定义mem_dump的一系列操作

--- common/utils/mem_dump.h
+++ common/utils/mem_dump.h
@@ -2,8 +2,30 @@
 #define _MEM_DUMP_H_
 
 #define MAX_PATH_LEN 100
+#define ROOT_PATH "/root/Workdir/hccl_test/log"
 
 void mem_dump(void *ptr, uint64_t len);
 void mem_dump_file(void *ptr, uint64_t len, char *file);
+
+// DUMP_DEBUG("allgather", "host_init", rank_id,
+//      buff_ptr, buff_size, 
+//      send_buff, send_malloc, send_trans, 
+//      recv_buff, recv_malloc, recv_trans);
+#ifdef MEM_DUMP
+    #define DUMP_DEBUG(FUNC_TAG, PTR_TAG, rank_id, buff_ptr, buff_size, send_buff, send_malloc, send_trans, recv_buff, recv_malloc, recv_trans) \
+    do { \
+        char bin_path[MAX_PATH_LEN]; \
+        memset(bin_path, 0, MAX_PATH_LEN); \
+        sprintf(bin_path, ROOT_PATH "/" FUNC_TAG "_" PTR_TAG "_rank_%d.bin", rank_id); \
+        mem_dump_file((char*)buff_ptr, buff_size, bin_path); \
+        printf("dump rank_id: %d >> " PTR_TAG "_buff: %p (malloc: %llu) send_buff: %p (malloc: %llu, trans: %llu), recv_buff: %p (malloc: %llu, trans: %llu), log_path: %s", \
+            rank_id, buff_ptr, (long long unsigned int)buff_size, \
+            send_buff, (long long unsigned int)send_malloc, (long long unsigned int)send_trans, \
+            recv_buff, (long long unsigned int)recv_malloc, (long long unsigned int)recv_trans, \
+            bin_path); \
+    } while (0)
+#else
+    #define DUMP_DEBUG(FUNC_TAG, PTR_TAG, rank_id, buff_ptr, buff_size, send_buff, send_malloc, send_trans, recv_buff, recv_malloc, recv_trans) void(0)
+#endif
 
 #endif
--- opbase_test/zhangdx_test.cc
+++ opbase_test/zhangdx_test.cc
@@ -7,14 +7,14 @@
 #include <cmath>
 #include <cstdint>
 #include <hccl/hccl_types.h>
-#include "hccl_brocast_rootinfo_test.h"
+#include "hccl_allgather_rootinfo_test.h"
 #include "hccl_opbase_rootinfo_base.h"
 #include "hccl_check_buf_init.h"
 using namespace hccl;
 
 HcclTest* init_opbase_ptr(HcclTest* opbase)
 {
-    opbase = new HcclOpBaseBrocastTest();
+    opbase = new hccl::HcclOpBaseAllgatherTest();
 
     return opbase;
 }
@@ -28,67 +28,60 @@ void delete_opbase_ptr(HcclTest* opbase)
 
 namespace hccl
 {
-HcclOpBaseBrocastTest::HcclOpBaseBrocastTest() : HcclOpBaseTest()
+HcclOpBaseAllgatherTest::HcclOpBaseAllgatherTest() : HcclOpBaseTest()
 {
     
     host_buf = nullptr;
     recv_buff_temp = nullptr;
     check_buf = nullptr;
-    buff = nullptr;
+    send_buff = nullptr;
+    recv_buff = nullptr;
 }
 
-HcclOpBaseBrocastTest::~HcclOpBaseBrocastTest()
+HcclOpBaseAllgatherTest::~HcclOpBaseAllgatherTest()
 {
 
 }
 
-int HcclOpBaseBrocastTest::init_buf_val()
+int HcclOpBaseAllgatherTest::init_buf_val()
 {
     //初始化校验内存
-    ACLCHECK(aclrtMallocHost((void**)&check_buf, malloc_kSize));
-    hccl_host_buf_init((char*)check_buf, data->count, dtype, val);
-
-    // dump初始化的内存
-    char bin_path[MAX_PATH_LEN];
-    memset(bin_path, 0, MAX_PATH_LEN);
-    sprintf(bin_path, "/root/Workdir/hccl_test/log/zhangdx_init_rank_%d.bin", rank_id);
-    printf("rank_id: %d, host_init_ptr: %p, len: %llu, log_path: %s\r\n", rank_id, check_buf, (long long unsigned int)malloc_kSize, bin_path);
-    mem_dump_file((char*)check_buf, malloc_kSize, bin_path);
-
+    ACLCHECK(aclrtMallocHost((void**)&check_buf, malloc_kSize * rank_size));
+    hccl_host_buf_init((char*)check_buf, data->count * rank_size, dtype, val);
     return 0;
 }
 
-int HcclOpBaseBrocastTest::check_buf_result()
+int HcclOpBaseAllgatherTest::check_buf_result()
 {
     //获取输出内存
-    ACLCHECK(aclrtMallocHost((void**)&recv_buff_temp, malloc_kSize));
-    ACLCHECK(aclrtMemcpy((void*)recv_buff_temp, malloc_kSize, (void*)buff, malloc_kSize, ACL_MEMCPY_DEVICE_TO_HOST));
+    ACLCHECK(aclrtMallocHost((void**)&recv_buff_temp, malloc_kSize * rank_size));
+    ACLCHECK(aclrtMemcpy((void*)recv_buff_temp, malloc_kSize * rank_size, (void*)recv_buff, malloc_kSize * rank_size, ACL_MEMCPY_DEVICE_TO_HOST));
     int ret = 0;
     switch(dtype)
     {
         case HCCL_DATA_TYPE_FP32:
-            ret = check_buf_result_float((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_float((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         case HCCL_DATA_TYPE_INT8:
         case HCCL_DATA_TYPE_UINT8:
-            ret = check_buf_result_int8((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_int8((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         case HCCL_DATA_TYPE_INT32:
         case HCCL_DATA_TYPE_UINT32:
-            ret = check_buf_result_int32((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_int32((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         case HCCL_DATA_TYPE_FP16:
         case HCCL_DATA_TYPE_INT16:
         case HCCL_DATA_TYPE_UINT16:
         case HCCL_DATA_TYPE_BFP16:
-            ret = check_buf_result_half((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_half((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         case HCCL_DATA_TYPE_INT64:
         case HCCL_DATA_TYPE_FP64:
-            ret = check_buf_result_int64((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_int64((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         case HCCL_DATA_TYPE_UINT64:
-            ret = check_buf_result_u64((char*)recv_buff_temp, (char*)check_buf, data->count);
+            ret = check_buf_result_u64((char*)recv_buff_temp, (char*)check_buf, data->count * rank_size);
             break;
         default:
             ret++;
@@ -100,27 +93,20 @@ int HcclOpBaseBrocastTest::check_buf_result()
         check_err++;
     }
 
-    // dump检查的内存
-    char bin_path[MAX_PATH_LEN];
-    memset(bin_path, 0, MAX_PATH_LEN);
-    sprintf(bin_path, "/root/Workdir/hccl_test/log/zhangdxt_check_rank_%d.bin", rank_id);
-    printf("rank_id: %d, host_check_ptr: %p, len: %llu, log_path: %s\r\n", rank_id, recv_buff_temp, (long long unsigned int)malloc_kSize, bin_path);
-    mem_dump_file((char*)recv_buff_temp, malloc_kSize, bin_path);
-
     return 0;
 }
 
-void HcclOpBaseBrocastTest::cal_execution_time(float time)
+void HcclOpBaseAllgatherTest::cal_execution_time(float time)
 {
-    double total_time_us = time * 1000;
-    double average_time_us = total_time_us / iters;
-    double algorithm_bandwith_GBytes_s = malloc_kSize / average_time_us * B_US_TO_GB_S;
+    double total_time_us              = time * 1000;
+    double average_time_us            = total_time_us / iters;
+    double algorithm_bandwith_GBytes_s = malloc_kSize * rank_size / average_time_us * B_US_TO_GB_S;
 
     print_execution_time(average_time_us, algorithm_bandwith_GBytes_s);
     return;
 }
 
-int HcclOpBaseBrocastTest::destory_check_buf()
+int HcclOpBaseAllgatherTest::destory_check_buf()
 {
     ACLCHECK(aclrtFreeHost(host_buf));
     ACLCHECK(aclrtFreeHost(recv_buff_temp));
@@ -128,32 +114,31 @@ int HcclOpBaseBrocastTest::destory_check_buf()
     return 0;
 }
 
-int HcclOpBaseBrocastTest::hccl_op_base_test() //主函数
+int HcclOpBaseAllgatherTest::hccl_op_base_test() //主函数
 {
     if (op_flag != 0 && rank_id == root_rank) {
         printf("Warning: The -o,--op <sum/prod/min/max> option does not take effect. Check the cmd parameter.\n");
     }
+    
     // 获取数据量和数据类型
     init_data_count();
 
+    data->count = (data->count + rank_size - 1) / rank_size;
     malloc_kSize = data->count * data->type_size;
 
     //申请集合通信操作的内存
-    ACLCHECK(aclrtMalloc((void**)&buff, malloc_kSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACLCHECK(aclrtMalloc((void**)&send_buff, malloc_kSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    ACLCHECK(aclrtMalloc((void**)&recv_buff, malloc_kSize * rank_size, ACL_MEM_MALLOC_HUGE_FIRST));
 
     //初始化输入内存
     ACLCHECK(aclrtMallocHost((void**)&host_buf, malloc_kSize));
-    if(rank_id == root_rank)
-    {
-        hccl_host_buf_init((char*)host_buf, data->count, dtype, val);
-        ACLCHECK(aclrtMemcpy((void*)buff, malloc_kSize, (void*)host_buf, malloc_kSize, ACL_MEMCPY_HOST_TO_DEVICE));
-    }
+    hccl_host_buf_init((char*)host_buf, data->count, dtype, val);
+    ACLCHECK(aclrtMemcpy((void*)send_buff, malloc_kSize, (void*)host_buf, malloc_kSize, ACL_MEMCPY_HOST_TO_DEVICE));
 
-    // dump NPU HBM Address
-    printf("rank_id: %d, buff_hbm_ptr: %p (size: %llu)\r\n",
-        rank_id,
-        buff,
-        (long long unsigned int)malloc_kSize);
+    DUMP_DEBUG("zhangdx", "host_init", rank_id, 
+        host_buf, malloc_kSize, 
+        send_buff, malloc_kSize, data->count,
+        recv_buff, malloc_kSize * rank_size, data->count);
 
     // 准备校验内存
     if (check == 1) {
@@ -162,13 +147,13 @@ int HcclOpBaseBrocastTest::hccl_op_base_test() //主函数
 
     //执行集合通信操作
     for(int j = 0; j < warmup_iters; ++j) {
-        HCCLCHECK(HcclBroadcast((void *)buff, data->count, (HcclDataType)dtype, root_rank, hccl_comm, stream));
+        HCCLCHECK(HcclAllGather((void *)send_buff, (void*)recv_buff, data->count, (HcclDataType)dtype, hccl_comm, stream));
     }
 
     ACLCHECK(aclrtRecordEvent(start_event, stream));
 
     for(int i = 0; i < iters; ++i) {
-        HCCLCHECK(HcclBroadcast((void *)buff, data->count, (HcclDataType)dtype, root_rank, hccl_comm, stream));
+        HCCLCHECK(HcclAllGather((void *)send_buff, (void*)recv_buff, data->count, (HcclDataType)dtype, hccl_comm, stream));
     }
     //等待stream中集合通信任务执行完成
     ACLCHECK(aclrtRecordEvent(end_event, stream));
@@ -184,8 +169,16 @@ int HcclOpBaseBrocastTest::hccl_op_base_test() //主函数
 
     cal_execution_time(time);
 
+    ACLCHECK(aclrtMallocHost((void**)&recv_buff_temp, malloc_kSize * rank_size));
+    ACLCHECK(aclrtMemcpy((void*)recv_buff_temp, malloc_kSize * rank_size, (void*)recv_buff, malloc_kSize * rank_size, ACL_MEMCPY_DEVICE_TO_HOST));
+    DUMP_DEBUG("zhangdx", "host_done", rank_id, 
+        recv_buff_temp, malloc_kSize * rank_size, 
+        send_buff, malloc_kSize, data->count,
+        recv_buff, malloc_kSize * rank_size, data->count);
+
     //销毁集合通信内存资源
-    ACLCHECK(aclrtFree(buff));
+    ACLCHECK(aclrtFree(send_buff));
+    ACLCHECK(aclrtFree(recv_buff));
     if (check == 1) {
         ACLCHECK(destory_check_buf());
     }
