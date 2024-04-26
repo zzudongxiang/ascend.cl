c0210a8bf913d44121adf345cfb1f62bd0ce9632
zzudongxiang <zzudongxiang@163.com>
2024-04-26 17:34:21 +0800

    优化显示信息

--- common/utils/mem_dump.h
+++ common/utils/mem_dump.h
@@ -25,16 +25,16 @@ void mem_dump_info(int rank_id, const char *func, void *ptr, uint64_t len, uint8
         sprintf(bin_path, ROOT_PATH "/" FUNC_TAG "_" PTR_TAG "_rank_%d.bin", rank_id);                                                         \
         mem_dump_file((char *)host_buff, host_malloc, bin_path);                                                                               \
         printf("func_" FUNC_TAG "_" PTR_TAG "_rank_%d >> log_path: %s\r\n", rank_id, bin_path);                                                \
-        printf("func_" FUNC_TAG "_" PTR_TAG "_rank_%d >> host_buff: %p (malloc: %.2f GB, value: %d)\r\n",                                      \
+        printf("func_" FUNC_TAG "_" PTR_TAG "_rank_%d >> host_buff: %p (malloc: %.2f GB, value: %f)\r\n",                                      \
                rank_id,                                                                                                                        \
-               host_buff, host_malloc * B_TO_GB, rank_id + 1);                                                                                 \
+               host_buff, host_malloc * B_TO_GB, (float)(rank_id + 1));                                                                        \
         printf("func_" FUNC_TAG "_" PTR_TAG "_rank_%d >> send_buff: %p (malloc: %.2f GB, trans: %.2f MB)\r\n",                                 \
                rank_id,                                                                                                                        \
                send_buff, send_malloc * B_TO_GB, send_trans * B_TO_MB);                                                                        \
         printf("func_" FUNC_TAG "_" PTR_TAG "_rank_%d >> recv_buff: %p (malloc: %.2f GB, trans: %.2f MB)\r\n",                                 \
                rank_id,                                                                                                                        \
                recv_buff, recv_malloc * B_TO_GB, recv_trans * B_TO_MB);                                                                        \
-        mem_dump_info(rank_id, FUNC_TAG, host_buff, host_malloc, DTYPE_LEN);                                                                                      \
+        mem_dump_info(rank_id, FUNC_TAG, host_buff, host_malloc, DTYPE_LEN);                                                                   \
     } while (0)
 
 // DUMP_INIT("allgather", rank_id,
@@ -45,13 +45,13 @@ void mem_dump_info(int rank_id, const char *func, void *ptr, uint64_t len, uint8
     do                                                                                                                                         \
     {                                                                                                                                          \
         DUMP_DEBUG(FUNC_TAG, "init", rank_id, host_buff, host_malloc, send_buff, send_malloc, send_trans, recv_buff, recv_malloc, recv_trans); \
-        printf("func_" FUNC_TAG "_init_rank_%d >> addr_shift: host(%p)->send(%p): %.2f GB\r\n",                                                \
+        printf("func_" FUNC_TAG "_init_rank_%d >> ptr_shift: %p (host) -> %p (send): %.2f GB\r\n",                                            \
                rank_id, host_buff, send_buff,                                                                                                  \
                ((double)*(u64 *)&host_buff - *(u64 *)&send_buff) * B_TO_GB);                                                                   \
-        printf("func_" FUNC_TAG "_init_rank_%d >> addr_shift: host(%p)->recv(%p): %.2f GB\r\n",                                                \
+        printf("func_" FUNC_TAG "_init_rank_%d >> ptr_shift: %p (host) -> %p (recv): %.2f GB\r\n",                                            \
                rank_id, host_buff, recv_buff,                                                                                                  \
                ((double)*(u64 *)&host_buff - *(u64 *)&recv_buff) * B_TO_GB);                                                                   \
-        printf("func_" FUNC_TAG "_init_rank_%d >> addr_shift: recv(%p)->send(%p): %.2f GB\r\n",                                                \
+        printf("func_" FUNC_TAG "_init_rank_%d >> ptr_shift: %p (recv) -> %p (send): %.2f GB\r\n",                                            \
                rank_id, recv_buff, send_buff,                                                                                                  \
                ((double)*(u64 *)&recv_buff - *(u64 *)&send_buff) * B_TO_GB);                                                                   \
                                                                                                                                                \
@@ -65,13 +65,13 @@ void mem_dump_info(int rank_id, const char *func, void *ptr, uint64_t len, uint8
     do                                                                                                                                          \
     {                                                                                                                                           \
         DUMP_DEBUG(FUNC_TAG, "done", rank_id, host_buff, host_malloc, send_buff, send_malloc, send_trans, recv_buff, recv_malloc, recv_trans);  \
-        printf("func_" FUNC_TAG "_done_rank_%d >> addr_shift: init(%p)->host(%p): %.2f GB\r\n",                                                 \
+        printf("func_" FUNC_TAG "_done_rank_%d >> ptr_shift: %p (init) -> %p (host): %.2f GB\r\n",                                             \
                rank_id, init_buff, host_buff,                                                                                                   \
                ((double)*(u64 *)&init_buff - *(u64 *)&host_buff) * B_TO_GB);                                                                    \
-        printf("func_" FUNC_TAG "_done_rank_%d >> addr_shift: init(%p)->send(%p): %.2f GB\r\n",                                                 \
+        printf("func_" FUNC_TAG "_done_rank_%d >> ptr_shift: %p (init) -> %p (send): %.2f GB\r\n",                                             \
                rank_id, init_buff, send_buff,                                                                                                   \
                ((double)*(u64 *)&init_buff - *(u64 *)&send_buff) * B_TO_GB);                                                                    \
-        printf("func_" FUNC_TAG "_done_rank_%d >> addr_shift: init(%p)->recv(%p): %.2f GB\r\n",                                                 \
+        printf("func_" FUNC_TAG "_done_rank_%d >> ptr_shift: %p (init) -> %p (recv): %.2f GB\r\n",                                             \
                rank_id, init_buff, recv_buff,                                                                                                   \
                ((double)*(u64 *)&init_buff - *(u64 *)&recv_buff) * B_TO_GB);                                                                    \
     } while (0)
